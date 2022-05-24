50ddf67ba67c1b948012ab33556e8debe5bb857a
szlove <webmaster@dooalab.com>
2022-05-25 02:14:20 +0900

    updated

---
+++ blockchain/transaction.go
@@ -0,0 +1,34 @@
+package blockchain
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Transaction struct {
+	senderBlockchainAddress    string
+	recipientBlockchainAddress string
+	value                      float32
+}
+
+func NewTransaction(sender, recipient string, value float32) *Transaction {
+	return &Transaction{sender, recipient, value}
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		SenderBlockchainAddress    string  `json:"sender_blockchain_address"`
+		RecipientBlockchainAddress string  `json:"recipient_blockchain_address"`
+		Value                      float32 `json:"value"`
+	}{
+		SenderBlockchainAddress:    t.senderBlockchainAddress,
+		RecipientBlockchainAddress: t.recipientBlockchainAddress,
+		Value:                      t.value,
+	})
+}
+
+func (t *Transaction) Print() {
+	fmt.Printf(" sender_blockchain_address       %s\n", t.senderBlockchainAddress)
+	fmt.Printf(" recipient_blockchain_address    %s\n", t.recipientBlockchainAddress)
+	fmt.Printf(" value                           %f\n", t.value)
+}
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/szlove/learnBlockchain2/blockchain"
+)
 
+func main() {
+	bc := blockchain.NewBlockchain("my_blockchain_address")
+	bc.AddTransaction("Slam", "Subin", 1.0)
+	bc.Mining()
+	bc.Print()
 }
